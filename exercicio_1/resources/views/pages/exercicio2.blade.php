6047a3fbfa580c609650c8f82cfdc4a324925023
Michael Vicente <michael.vicente@engeselt.com.br>
2022-03-17 14:20:32 -0300

    adição de controladoras e resolução dos exercicios da atividade 1 - incompleto ainda

---
+++ exercicio_1/resources/views/pages/exercicio2.blade.php
@@ -0,0 +1,7 @@
+@extends('template.template')
+@section('title')
+    Laravel - Exercicio 2
+@endsection
+@section('body')
+
+@endsection
---
+++ exercicio_1/resources/views/pages/exercicio3.blade.php
@@ -0,0 +1,7 @@
+@extends('template.template')
+@section('title')
+    Laravel - Exercicio 3
+@endsection
+@section('body')
+
+@endsection
